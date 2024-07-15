291aee9fe394275139533cacf4ab19bd5ab05470
NguyenCuong1509 <cuongnmph51410@gmail.com>
2024-07-15 13:07:40 +0700

    dg

--- PRL/NhanVien_HoaDon.Designer.cs
+++ PRL/NhanVien_HoaDon.Designer.cs
@@ -28,12 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "NhanVien_HoaDon";
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox5 = new TextBox();
+            label6 = new Label();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(1, 281);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(834, 370);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            label1.Location = new Point(33, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 25);
+            label1.TabIndex = 1;
+            label1.Text = "ID Hóa Đơn";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            label2.Location = new Point(33, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(158, 25);
+            label2.TabIndex = 1;
+            label2.Text = "ID Khách hàng";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            label3.Location = new Point(33, 109);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 25);
+            label3.TabIndex = 1;
+            label3.Text = "ID nhân viên";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            label4.Location = new Point(33, 152);
+            label4.Name = "label4";
+            label4.Size = new Size(137, 25);
+            label4.TabIndex = 1;
+            label4.Text = "Ngày lập đơn";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            label5.Location = new Point(33, 197);
+            label5.Name = "label5";
+            label5.Size = new Size(102, 25);
+            label5.TabIndex = 2;
+            label5.Text = "Tổng tiền";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(228, 6);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 31);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(228, 55);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(300, 31);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(228, 106);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(300, 31);
+            textBox3.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(228, 152);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 31);
+            dateTimePicker1.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(228, 194);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(300, 31);
+            textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(590, 7);
+            button1.Name = "button1";
+            button1.Size = new Size(207, 45);
+            button1.TabIndex = 8;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(590, 58);
+            button2.Name = "button2";
+            button2.Size = new Size(207, 49);
+            button2.TabIndex = 9;
+            button2.Text = "Sửa";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(590, 113);
+            button3.Name = "button3";
+            button3.Size = new Size(207, 47);
+            button3.TabIndex = 10;
+            button3.Text = "Xóa";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(228, 244);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(593, 31);
+            textBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 11F, FontStyle.Bold);
+            label6.Location = new Point(33, 244);
+            label6.Name = "label6";
+            label6.Size = new Size(103, 25);
+            label6.TabIndex = 2;
+            label6.Text = "Tìm kiếm";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(590, 169);
+            button4.Name = "button4";
+            button4.Size = new Size(209, 56);
+            button4.TabIndex = 12;
+            button4.Text = "Mở hóa đơn chi tiết";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // NhanVien_HoaDon
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(833, 647);
+            Controls.Add(button4);
+            Controls.Add(textBox5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "NhanVien_HoaDon";
+            Text = "NhanVien_HoaDon";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox5;
+        private Label label6;
+        private Button button4;
     }
 }
